b29be47ba751297c85b4fc4afd9ff8265cfe9fde
jsand3 <sanders3.j@gmail.com>
2018-02-17 11:13:37 -0600

    Saved Images and Video in project folder

--- assets/script.js
+++
@@ -1,2 +0,0 @@
-// var cube,square;square=function(e){return e*e},cube=function(e){return square(e)*e},console.log("Hello World from CoffeeScript");
-// console.log("Hello World from Main Javascript");
